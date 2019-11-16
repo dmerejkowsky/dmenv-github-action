da9d613378e4d6bf6f2ec65144eb821d3078a0b9
Dimitri Merejkowsky <d.merej@gmail.com>
2019-11-16 14:49:02 +0100

    1

--- index.js
+++ index.js
@@ -2,18 +2,12 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 async function run()
-    try {
-      // `who-to-greet` input defined in action metadata file
-      const nameToGreet = core.getInput('who-to-greet');
-      console.log(`Hello ${nameToGreet}!`);
-      const time = (new Date()).toTimeString();
-      core.setOutput("time", time);
-      // Get the JSON webhook payload for the event that triggered the workflow
-      const payload = JSON.stringify(github.context.payload, undefined, 2)
-      console.log(`The event payload: ${payload}`);
-    } catch (error) {
-      core.setFailed(error.message);
-    }
+  try {
+    const dmenvVersion = core.getInput("dmenv-version");
+    console.log(`Installing dmenv version ${dmenvVersion}`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
 
 run()
