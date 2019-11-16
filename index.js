24f201c87fddce1c757194459ac112b0bdb947de
Dimitri Merejkowsky <d.merej@gmail.com>
2019-11-16 14:40:39 +0100

    no cache

--- index.js
+++ index.js
@@ -23,8 +23,7 @@ async function run() {
         }
       });
       await exec.exec(dmenvBinPath, ["--version"]);
-        console.log(`dmenv version is ${dmenvVersion}`);
-      await tc.cacheFile(dmenvBinPath, 'dmenv', 'dmenv', dmenvVersion);
+      //await tc.cacheFile(dmenvBinPath, 'dmenv', 'dmenv', dmenvVersion);
     }
   } catch (error) {
     core.setFailed(error.message);
