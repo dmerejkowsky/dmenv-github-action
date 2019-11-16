e68717641f21b764d38403fb96762dba57d30c5f
Dimitri Merejkowsky <d.merej@gmail.com>
2019-11-16 14:06:05 +0100

    Trying to run dmenv --version and cache the binary

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
+const fs = require('fs');
 const core = require('@actions/core');
+const tc = require('@actions/tool-cache');
+const io = require('@actions/io');
+const exec = require('@actions/exec');
 
-
-try {
-  const dmenvVersion = core.getInput("dmenv-version");
-  console.log(`Installing dmenv version ${dmenvVersion}`);
-} catch (error) {
-  core.setFailed(error.message);
+async function run() {
+  try {
+    const dmenvVersion = core.getInput("dmenv-version");
+    console.log(`Installing dmenv version ${dmenvVersion}`);
+    // Todo: darwin, win32
+    if (process.platform === 'linux') {
+      const dmenvDlPath = await tc.downloadTool(`https://github.com/TankerHQ/dmenv/releases/download/v${dmenvVersion}/dmenv-linux`);
+      const binariesPath = `${process.env.HOME}/.local/bin`;
+      core.addPath(binariesPath);
+      await io.mkdirP(binariesPath);
+      const dmenvBinPath = `${binariesPath}/dmenv'`;
+      console.log(`cp ${dmenvDlPath} -> ${dmenvBinPath}`);
+      await io.cp(dmenvDlPath, dmenvBinPath);
+      await fs.chmod(dmenvBinPath, 0o775, (err) => {
+        if (err) {
+          throw err;
+        }
+      });
+      await exec.exec(dmenvBinPath, ["--version"]);
+      await tc.cacheFile(dmenvBinPath);
+    }
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+run()
